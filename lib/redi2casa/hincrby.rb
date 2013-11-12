3f4beab2148fc98f2dcc5efe2f9fb5e2ea12755c
viresh <asviresh@gmail.com>
2013-11-12 08:13:54 +0530

    hincrby defaults to 1 when you dont pass value

--- lib/redi2casa/hincrby.rb
+++ lib/redi2casa/hincrby.rb
@@ -1,5 +1,5 @@
 class Redi2casa
-  def hincrby namespace, key, value
+  def hincrby namespace, key, value = 1
     query = "update counters set value = value + #{value} where key='#{namespace}' and column1 = '#{key}'"
     @db_conn.execute(query)
   end
