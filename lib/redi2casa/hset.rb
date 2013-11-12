6aa38613dfa0ef22b947f07cf7c8595eb5746cd9
viresh <asviresh@gmail.com>
2013-11-12 14:33:02 +0530

    type conversion in hset

--- lib/redi2casa/hset.rb
+++ lib/redi2casa/hset.rb
@@ -1,6 +1,6 @@
 class Redi2casa
   def hset namespace, key, value
-    hash = {key => value}
+    hash = {key.to_s => value.to_s}
     @db_conn.execute("UPDATE sets SET key = key + ? WHERE namespace = ?", hash, namespace)
   end
 end
