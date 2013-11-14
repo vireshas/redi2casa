8237e659f3d48619be062769f38bb7f99ee408a6
viresh <asviresh@gmail.com>
2013-11-14 19:44:13 +0530

    changing hget

--- lib/redi2casa.rb
+++ lib/redi2casa.rb
@@ -1,9 +1,15 @@
 require "redi2casa/version"
-require "cassandra-cql"
+require "cql"
+
 Gem.find_files("redi2casa/*.rb").each { |path| require path }
 
 class Redi2casa
   def initialize(host, keyspace)
-    @db_conn =  CassandraCQL::Database.new(host, keyspace)
+    @db_conn =  Cql::Client.connect(host)
+    @db_conn.use(keyspace)
+  end
+
+  def parse_response(resp, key)
+    resp.map {|entry| entry[key].to_s }.first
   end
 end
--- lib/redi2casa/hget.rb
+++ lib/redi2casa/hget.rb
@@ -1,11 +1,11 @@
 class Redi2casa
   def hget namespace, key, type = "counters"
     if type.to_s == "hash"
-      resp = @db_conn.execute("select * from sets where namespace = ?", namespace).fetch
-      resp.nil? ? nil : resp.to_hash['key'][key]
+      resp = @db_conn.execute("select key from hashes where namespace = '#{namespace}'")
+      parse_response(resp, "key")
     elsif type.to_s == "counters"
-      response = @db_conn.execute("select * from counters where KEY='#{namespace}' and column1 = '#{key}'")
-      response.fetch {|entry| return entry.to_hash["value"]}
+      resp = @db_conn.execute("select value from counters where KEY='#{namespace}' and column1 = '#{key}'")
+      parse_response(resp, "value")
     end
   end
 end
