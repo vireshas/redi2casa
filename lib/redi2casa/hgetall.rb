13112d8cea0ed75470d45df20ec37f62e066ffcb
viresh <asviresh@gmail.com>
2013-11-14 19:44:35 +0530

    changing hgetall

--- lib/redi2casa/hgetall.rb
+++ lib/redi2casa/hgetall.rb
@@ -2,12 +2,10 @@ class Redi2casa
   def hgetall key, type = "counters"
     if type.to_s == "counters"
       response = @db_conn.execute("select * from counters where KEY='#{key}'")
-      hsh = {}
-      response.fetch {|resp| entry = resp.to_hash; hsh[entry["column1"]] = entry["value"] }
-      hsh
+      response.inject({}) {|hsh, entry| hsh[entry["column1"]] = entry["value"]; hsh}
     elsif type.to_s == "hash"
-      resp = @db_conn.execute("select * from sets where namespace = ?", key).fetch
-      resp.nil? ? nil : resp.to_hash['key']
+      resp = @db_conn.execute("select key from hashes where namespace = '#{key}'")
+      parse_response(resp, "key")
     end
   end
 end
