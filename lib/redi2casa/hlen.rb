6e50dc42a9a8ccce2b365d78c571984e0968d625
rajofchennai <rajofchennai@yahoo.com>
2013-12-09 15:43:27 +0530

    hlen to support counters

--- lib/redi2casa/hlen.rb
+++ lib/redi2casa/hlen.rb
@@ -1,7 +1,12 @@
 class Redi2casa
   # Supports only hashes now
-  def hlen key
-    response = @db_conn.execute("select count(*) from hashes where key = '#{key}'")
-    parse_response(response, "count").to_i
+  def hlen key, type = "hash"
+    if type == "hash"
+      response = @db_conn.execute("select count(*) from hashes where key = '#{key}'")
+      parse_response(response, "count").to_i
+    elsif type == "counter"
+      response = @db_conn.execute("select count(*) from counters where key = '#{key}'")
+      parse_response(response, "count").to_i
+    end
   end
 end
