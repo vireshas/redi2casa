8a8e5d1babfe2fbd1be47ce1a63f41ed041aaffb
rajofchennai <rajofchennai@yahoo.com>
2013-12-10 11:22:37 +0530

    fix bug in del using wrong table name

--- lib/redi2casa/del.rb
+++ lib/redi2casa/del.rb
@@ -1,6 +1,16 @@
 class Redi2casa
   ## Type can be sets, hashes, lists, keyvalue
   def del key, type
-    @db_conn.execute("delete from #{type} where key = '#{key}'")
+    if type == "counter"
+      @db_conn.execute("delete from counters where key = '#{key}'")
+    elsif type == 'keyvalue'
+      @db_conn.execute("delete from keyvalues where key = '#{key}'")
+    elsif type == 'hash'
+      @db_conn.execute("delete from hashes where key = '#{key}'")
+    elsif type == 'list'
+      @db_conn.execute("delete from lists where key = '#{key}'")
+    elsif type == 'set'
+      @db_conn.execute("delete from sets where key = '#{key}'")
+    end
   end
 end
