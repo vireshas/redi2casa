f4da6ae37a283753c6fe74e88a56593c4335f3ae
rajofchennai <rajofchennai@yahoo.com>
2013-12-10 14:03:58 +0530

    fix setbit bug

--- lib/redi2casa/set_bit.rb
+++ lib/redi2casa/set_bit.rb
@@ -1,7 +1,7 @@
 class Redi2casa
   # FIXME:This could potentially lead to read write problem.
   def set_bit key, offset, bit
-    if bit != 0 || bit != 1
+    if !(bit != 0 || bit != 1)
       raise RuntimeError.new("Invalid bit")
     end
     set("#{key}:#{offset}", bit)
