b8a6bc00188025125f826fc9b82a2d8784bccbe3
rajofchennai <rajofchennai@yahoo.com>
2013-12-17 11:45:48 +0530

    Support hmget and update version

---
+++ lib/redi2casa/hmget.rb
@@ -0,0 +1,22 @@
+class Redi2casa
+  def hmget key, type, *column1s
+    if type == "counter"
+      table = "counters"
+    elsif type == "hash"
+      table = "hashes"
+    else
+      raise RuntimeError.new("invalid table")
+    end
+
+    resp = @db_conn.execute("select column1, value from #{table} where key = '#{key}' and column1 IN ('#{column1s.join("','")}')")
+    hash = {}
+    resp.each do |r|
+      hash[r["column1"]] = r["value"]
+    end
+    result = []
+    column1s.each do |column1|
+      result << hash[column1.to_s]
+    end
+    result
+  end
+end
