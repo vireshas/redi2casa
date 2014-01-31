bc383ab4adb3c027a700faf7396a42cd9d766b5d
rajofchennai <rajofchennai@yahoo.com>
2014-01-31 11:45:59 +0530

    fix bug in non-string values of hset

--- lib/redi2casa/hset.rb
+++ lib/redi2casa/hset.rb
@@ -1,5 +1,5 @@
 class Redi2casa
   def hset key, column1, value
-    execute("UPDATE hashes SET value = ? WHERE column1 = ? and key = ?", value, column1.to_s, key.to_s)
+    execute("UPDATE hashes SET value = ? WHERE column1 = ? and key = ?", value.to_s, column1.to_s, key.to_s)
   end
 end
