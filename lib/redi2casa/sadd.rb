cdcf2a644a33e82e0956356a6737144d0bb7dab5
rajofchennai <rajofchennai@yahoo.com>
2014-02-03 17:11:08 +0530

    fix sadd with array

--- lib/redi2casa/sadd.rb
+++ lib/redi2casa/sadd.rb
@@ -1,5 +1,6 @@
 class Redi2casa
   def sadd key, *members
+    members.flatten!
     members.collect! { |member|  member.to_s }
     execute("update sets set members = members + ? where key = ?", members, key.to_s)
   end
--- lib/redi2casa/sadd_with_expire.rb
+++ lib/redi2casa/sadd_with_expire.rb
@@ -1,5 +1,7 @@
 class Redi2casa
   def sadd_with_expire( key, ttl, *members)
+    members.flatten!
+    members.collect! { |member|  member.to_s }
     execute("update sets using ttl ? set members = members + ? where key = ?", ttl, members, key.to_s)
   end
 end
