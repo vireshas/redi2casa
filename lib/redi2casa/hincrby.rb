cd4d15668da7f6b9cc7e52450e6b9be3cb2f965c
rajofchennai <rajofchennai@yahoo.com>
2014-02-03 17:23:26 +0530

    fix bug with counter when the number given is not int

--- lib/redi2casa/hincrby.rb
+++ lib/redi2casa/hincrby.rb
@@ -1,5 +1,6 @@
 class Redi2casa
   def hincrby key, column1, value = 1
+    value = value.to_i
     execute("update counters set value = value + ? where key = ? and column1 = ?", value, key.to_s, column1.to_s)
   end
 end
--- lib/redi2casa/version.rb
+++ lib/redi2casa/version.rb
@@ -1,3 +1,3 @@
 class Redi2casa
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
 end
