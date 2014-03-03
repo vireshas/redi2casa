c2007b3e2c0c61f023773caca2a4bac838580d59
rajofchennai <rajofchennai@yahoo.com>
2014-03-03 12:03:22 +0530

    zrangebyscore range is inclusive

--- lib/redi2casa/version.rb
+++ lib/redi2casa/version.rb
@@ -1,3 +1,3 @@
 class Redi2casa
-  VERSION = "0.2.12"
+  VERSION = "0.2.13"
 end
--- lib/redi2casa/zrangebyscore.rb
+++ lib/redi2casa/zrangebyscore.rb
@@ -2,9 +2,9 @@ class Redi2casa
   # Ranges are exclusive
   def zrangebyscore key, lower_limit, higher_limit = -1
     if higher_limit == -1
-      resp = execute("select value from sorted_sets where key = ? and score > ?", key.to_s, lower_limit)
+      resp = execute("select value from sorted_sets where key = ? and score >= ?", key.to_s, lower_limit)
     else
-      resp = execute("select value from sorted_sets where key = ? and score > ? and score < ?", key.to_s, lower_limit, higher_limit)
+      resp = execute("select value from sorted_sets where key = ? and score >= ? and score <= ?", key.to_s, lower_limit, higher_limit)
     end
     resp.collect do |entry|
       entry['value']
